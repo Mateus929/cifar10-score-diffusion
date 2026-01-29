6155e50594dc8f9ef9d75bd042108a77795b1bd2
mate <zhorzholianimate@gmail.com>
2026-01-29 21:57:08 +0400

    new model for ncsn

--- utils/ncsn_utils.py
+++ utils/ncsn_utils.py
@@ -22,7 +22,7 @@ def score_matching_loss(model, x, noise_scales):
 
     noise = torch.randn_like(x)
     x_noisy = q_sample(x, noise_scale_batch, noise=noise)
-    score = model(x_noisy, noise_scale_batch)
+    score = model(x_noisy, noise_scale_idx)
 
     loss = F.mse_loss(noise_scale_batch * score, -noise)
 
